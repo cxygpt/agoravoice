24eac431153c6e858a734c5c63ca74d3a4ae8cf7
xwh <xiongwenhao@71live.com>
2023-04-06 12:19:09 +0800

    修改gradle版本

--- app/src/main/java/com/adance/agoravoice/MainActivity.kt
+++ app/src/main/java/com/adance/agoravoice/MainActivity.kt
@@ -2,7 +2,6 @@ package com.adance.agoravoice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.scifate.moudle_voice.rtm.RTMSDKManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
